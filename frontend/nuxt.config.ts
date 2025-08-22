ea1cb6d0a772563a56bf32bc46c9ffffdc292c6f
Kiady <rambelosonkiady@gmail.com>
2025-08-22 16:25:23 +0300

    add Tailwind CSS integration and main stylesheet

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui']
 })
